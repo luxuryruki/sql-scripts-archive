3daea4c45df01a9f9a8f2b81f221201f9d7c7128
KANG JIHYO <luxuryjihyo@naver.com>
2024-07-06 14:18:23 +0900

    first commit

---
+++ src/main/java/com/example/sqlscriptarchive/SqlScriptArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlscriptarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlScriptArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlScriptArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sqlscriptarchive/SqlScriptArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlscriptarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlScriptArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
